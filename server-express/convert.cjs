edcedf943f4e67fb63784859ea7e5bf080f4fa25
Shannon <svloh1@student.monash.edu>
2024-07-15 02:33:29 +1000

    add functions to extract data from word doc

--- server-express/convert.cjs
+++ server-express/convert.cjs
@@ -4,7 +4,7 @@ const { readStyles } = require("./utils/unzipDocx");
 const Publication = require("./models/publication");
 
 const main = async () => {
-    const publicationId = "2024-07-13_02-26-29_5043-anrows-fitzgibbon-rr2-v1e-client-reviewed";
+    const publicationId = "2024-07-13_09-08-01_5043-anrows-fitzgibbon-rr2-v1e-client-reviewed";
     const publication = new Publication(publicationId);
     await publication.setProperties();
     console.log(publication.print());
@@ -14,7 +14,10 @@ const main = async () => {
     const unzippedWordFolder = `${publication.folder}/input/source`;
 
     const styleMap = await wordDocument.getStyleMap(unzippedWordFolder);
-    console.log(styleMap);
+
+    const css = await wordDocument.getCssCode(unzippedWordFolder);
+
+    console.log(css);
 
     // // create the stylesheet from unzipped word
 
--- server-express/utils/cssFromWord.js
+++ server-express/utils/cssFromWord.js
@@ -1,104 +1,157 @@
 const { readStyles } = require("./unzipDocx");
 
+function getColour(styleId, styles) {
+    const styleObject = styles.find((style) => style["$"]["w:styleId"] === styleId);
+    try {
+        const colourId = styleObject["w:rPr"][0]["w:color"][0]["$"]["w:val"];
+        // console.log(styleId, colourId);
+
+        return colourId;
+    } catch {
+        try {
+            const parentStyleId = styleObject["w:basedOn"][0]["$"]["w:val"];
+            // console.log(styleId, parentStyleId);
+            return getColour(parentStyleId, styles, cssObject);
+        } catch {
+            // do nothing
+        }
+    }
+}
+
+function getFontSize(styleId, styles) {
+    const styleObject = styles.find((style) => style["$"]["w:styleId"] === styleId);
+
+    if (styleId === "Hyperlink") {
+        return "inherit";
+    }
+
+    try {
+        const fontSize = styleObject["w:rPr"][0]["w:sz"][0]["$"]["w:val"];
+        let ptSize = parseInt(fontSize) / 2;
+        return convertPtToRem(ptSize);
+    } catch {}
+}
+
+function convertPtToRem(ptSize) {
+    if (10 < ptSize && ptSize <= 12) {
+        return "1rem";
+    }
+
+    if (ptSize <= 10) {
+        return "0.9rem";
+    }
+
+    if (ptSize > 12 && ptSize <= 14) {
+        return "1.2rem";
+    }
+
+    if (ptSize > 14 && ptSize <= 20) {
+        return "1.5rem";
+    }
+
+    if (ptSize > 20 && ptSize < 30) {
+        return "1.8rem";
+    }
+
+    if (ptSize >= 30) {
+        return "2.5rem";
+    }
+
+    return ptSize;
+}
+
+function getFontWeight(styleId, styles) {
+    const styleObject = styles.find((style) => style["$"]["w:styleId"] === styleId);
+    try {
+        const bold = styleObject["w:rPr"][0]["w:b"][0];
+        if (bold === "") {
+            return "bold";
+        } else {
+            const boldValue = bold["$"]["w:val"];
+            if (boldValue === "0") {
+                return "normal";
+            }
+        }
+    } catch {
+        // do nothing
+    }
+}
+
+function getIndentation(styleId, styles) {
+    const styleObject = styles.find((style) => style["$"]["w:styleId"] === styleId);
+    try {
+        const indentObject = styleObject["w:pPr"][0]["w:ind"][0]["$"];
+        return {
+            left: indentObject["w:left"],
+            right: indentObject["w:right"],
+            hanging: indentObject["w:hanging"],
+        };
+        // TO DO: Add first line value
+    } catch {
+        try {
+            const parentStyleId = styleObject["w:basedOn"][0]["$"]["w:val"];
+            return getIndentation(parentStyleId, styles);
+        } catch {
+            // do nothing
+        }
+    }
+}
+
+function setIndentation(cssObject, selector, indentation) {
+    if (indentation["left"]) {
+        cssObject[selector]["margin-left"] = indentation["left"] + "px";
+    }
+}
+
 async function createCssObject(unzippedWordFolder, styleMapObjects) {
-	const styles = await readStyles(unzippedWordFolder);
+    const styles = await readStyles(unzippedWordFolder);
 
-	const cssObject = {};
+    const cssObject = {};
 
-	// create css object
-	for (const object of styleMapObjects) {
-		// get the styleObject from styles based on the id
-		const styleObject = styles.find((style) => style["$"]["w:styleId"] === object.id);
-		const selector = "." + object.className;
-		cssObject[selector] = {};
+    // create css object
+    for (const object of styleMapObjects) {
+        const selector = "." + object.className;
+        cssObject[selector] = {};
 
-		// get the colour of the style
-		try {
-			const colour = styleObject["w:rPr"][0]["w:color"][0]["$"]["w:val"];
-			const cssColour = "#" + colour.trim();
+        // get the colour of the style
+        const colourId = getColour(object.id, styles);
+        if (colourId && colourId !== "auto" && colourId !== "000000") {
+            cssObject[selector]["color"] = `#${colourId}`;
+        }
 
-			if (cssColour !== "#000000" && cssColour !== "#auto") {
-				cssObject[selector]["color"] = cssColour;
-			}
-		} catch {
-			// do nothing
-		}
+        // get font size
+        const fontSize = getFontSize(object.id, styles);
+        if (fontSize) {
+            cssObject[selector]["font-size"] = fontSize;
+        }
 
-		// get font size
-		try {
-			const fontSize = styleObject["w:rPr"][0]["w:sz"][0]["$"]["w:val"];
-			let ptSize = parseInt(fontSize) / 2;
-			const cssFontSize = getFontSize(ptSize, object.id);
-			cssObject[selector]["font-size"] = cssFontSize;
-		} catch {
-			// do nothing
-		}
+        // get font weight
+        const fontWeight = getFontWeight(object.id, styles);
+        if (fontWeight) {
+            cssObject[selector]["font-weight"] = fontWeight;
+        }
 
-		// get font weight
-		try {
-			const bold = styleObject["w:rPr"][0]["w:b"][0];
-			if (bold === "") {
-				cssObject[selector]["font-weight"] = "bold";
-			} else {
-				const boldValue = bold["$"]["w:val"];
-				if (boldValue === "0") {
-					cssObject[selector]["font-weight"] = "normal";
-				}
-			}
-		} catch {
-			// do nothing
-		}
-	}
+        const indentation = getIndentation(object.id, styles);
+        if (indentation) setIndentation(cssObject, selector, indentation);
+    }
 
-	return cssObject;
+    return cssObject;
 }
 
 function generateCssCode(cssObject) {
-	let cssCode = "";
-	for (const selector in cssObject) {
-		cssCode += `${selector} {\n`;
-		for (const property in cssObject[selector]) {
-			cssCode += `    ${property}: ${cssObject[selector][property]};\n`;
-		}
-		cssCode += "}\n\n";
-	}
+    let cssCode = "";
+    for (const selector in cssObject) {
+        cssCode += `${selector} {\n`;
+        for (const property in cssObject[selector]) {
+            cssCode += `    ${property}: ${cssObject[selector][property]};\n`;
+        }
+        cssCode += "}\n\n";
+    }
 
-	return cssCode;
-}
-
-function getFontSize(ptSize, objectId) {
-	if (objectId === "Hyperlink") {
-		return "inherit";
-	}
-
-	if (10 < ptSize && ptSize <= 12) {
-		return "1rem";
-	}
-
-	if (ptSize <= 10) {
-		return "0.9rem";
-	}
-
-	if (ptSize > 12 && ptSize <= 14) {
-		return "1.2rem";
-	}
-
-	if (ptSize > 14 && ptSize <= 20) {
-		return "1.5rem";
-	}
-
-	if (ptSize > 20 && ptSize < 30) {
-		return "1.8rem";
-	}
-
-	if (ptSize >= 30) {
-		return "2.5rem";
-	}
-
-	return ptSize;
+    return cssCode;
 }
 
 module.exports = {
-	createCssObject,
-	generateCssCode
+    createCssObject,
+    generateCssCode,
 };
